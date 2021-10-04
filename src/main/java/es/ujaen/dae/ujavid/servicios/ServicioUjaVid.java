867dbab5af14765b35252257821e497a02160026
Antonio Manuel Venzala Campaña <amvc0011@red.ujaen.es>
2021-10-04 20:58:57 +0200

    fix 2

--- src/main/java/es/ujaen/dae/ujavid/servicios/ServicioUjaVid.java
+++ src/main/java/es/ujaen/dae/ujavid/servicios/ServicioUjaVid.java
@@ -4,10 +4,25 @@
  */
 package es.ujaen.dae.ujavid.servicios;
 
+import es.ujaen.dae.ujavid.entidades.Rastreador;
+import es.ujaen.dae.ujavid.entidades.Usuario;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author admin
  */
+@Service
 public class ServicioUjaVid {
+    Map<String, Rastreador> rastreadores;
+    Map<UUID,Usuario> usuarios;
+
+    public ServicioUjaVid() {
+        rastreadores= new TreeMap<>();
+        usuarios = new TreeMap<>();
+    }
     
 }

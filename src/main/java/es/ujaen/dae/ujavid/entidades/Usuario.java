60cd8b232f7d9bf31fe4fa58655bf606dc6a0767
Adrian Perez Ortega <apo00015@red.ujaen.es>
2021-10-09 11:30:04 +0200

    - Corrección constructor Usuario - Inserccion password Usuario

--- src/main/java/es/ujaen/dae/ujavid/entidades/Usuario.java
+++ src/main/java/es/ujaen/dae/ujavid/entidades/Usuario.java
@@ -6,6 +6,7 @@ package es.ujaen.dae.ujavid.entidades;
 
 import java.time.LocalDateTime;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,31 +15,64 @@ import java.util.UUID;
  * @author admin
  */
 public class Usuario {
-    /**UUID del usuario**/ 
+
+    /**
+     * UUID del usuario*
+     */
     UUID uuid;
-    /**Número de teléfono**/
+
+    /**
+     * Número de teléfono*
+     */
     String numTelefono;
-    /**Fecha de alta**/
+
+    /**
+     * Fecha de alta*
+     */
     LocalDate f_alta;
-    /**Fecha de positivo **/
+
+    /**
+     * Fecha de positivo *
+     */
     LocalDateTime f_positivo;
-    /**¿Es Positivo?**/
+
+    /**
+     * ¿Es Positivo?*
+     */
     boolean positivo;
-    
-    /**Listado de Contactos Cercanos**/
+
+    /**
+     * Cotraseña del usuario
+     */
+    private String password;
+
+    /**
+     * Listado de Contactos Cercanos*
+     */
     List<ContactoCercano> listadoContactos;
 
-    public Usuario(String numTelefono, LocalDate f_alta, LocalDateTime f_positivo, boolean positivo, List<ContactoCercano> listadoContactos) {
+    /**
+     * Constructor parametrizado de al clase Usuario
+     *
+     * @param numTelefono Nº de teléfono
+     * @param f_alta Fecha en la que se registra el usuario
+     */
+    public Usuario(String numTelefono, String password, LocalDate f_alta) {
         this.uuid = UUID.randomUUID();
+        this.password = password;
         this.numTelefono = numTelefono;
         this.f_alta = f_alta;
-        this.f_positivo = f_positivo;
-        this.positivo = positivo;
-        this.listadoContactos = listadoContactos;
+        this.f_positivo = null;
+        this.positivo = false;
+        this.listadoContactos = new ArrayList<>();
     }
 
     public UUID getUuid() {
         return uuid;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getNumTelefono() {
@@ -53,15 +87,21 @@ public class Usuario {
         return f_positivo;
     }
 
+    public void setF_positivo(LocalDateTime f_positivo) {
+        this.f_positivo = f_positivo;
+    }
+
     public boolean isPositivo() {
         return positivo;
+    }
+
+    public void setPositivo(boolean positivo) {
+        this.positivo = positivo;
     }
 
     public List<ContactoCercano> getListadoContactos() {
         return listadoContactos;
     }
-    
-    
-    
+
     
 }
